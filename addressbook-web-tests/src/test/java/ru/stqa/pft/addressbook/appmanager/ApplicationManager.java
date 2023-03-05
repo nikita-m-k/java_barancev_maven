3323eab92c6be2c65a58259edbcb422bb656eecf
nikita-m-k <nikitasc2@gmail.com>
2023-03-05 14:29:41 +0300

    создал возможность выбора браузера для запуска тестов + конструктор в апликэйшн менеджере для передачи в параметры браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,20 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private Browser browser;
+
+  public ApplicationManager(Browser browser) {
+    this.browser=browser;
+  }
 
   public void init() throws InterruptedException {
-    wd = new FirefoxDriver();
+    if (browser == Browser.FIREFOX){
+      wd = new FirefoxDriver();
+    } else if (browser == Browser.CHROME){
+      wd = new ChromeDriver();
+    } else if (browser == Browser.EDGE){
+      wd = new EdgeDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     Thread.sleep(1000);
