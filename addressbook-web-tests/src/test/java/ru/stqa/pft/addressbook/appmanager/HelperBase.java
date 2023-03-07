82df7c1aabe1e8d8a696db23dfd949ea1a872c70
nikita-m-k <nikitasc2@gmail.com>
2023-03-07 11:47:41 +0300

    Доработка вспомогательного метода, проверками на идентичность текста(увеличение скорости)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -16,9 +16,14 @@ public class HelperBase {
   }
 
   protected void type(By locator, String text) {
-    wd.findElement(locator).clear();
     click(locator);
-    wd.findElement(locator).sendKeys(text);
+    if (text != null){
+      String existingText =wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)){
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
   }
   public boolean isAlertPresent(){
     try {
